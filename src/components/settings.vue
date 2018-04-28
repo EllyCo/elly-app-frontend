187810b9215a9a82f6dc07c9eeb6b099244e58e3
Bradley Hall <bradleyhall@169-231-142-222.wireless.ucsb.edu>
2018-04-28 09:01:02 -0700

    started settings compnent

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,11 +1,31 @@
 <template>
   <div id="settings">
-    As a test, the following is from the settings component:
-    <ol>
-        <li>Setting #1 value: {{ setting1 }}</li>
-        <li>Setting #2 value: {{ setting2 }}</li>
-    </ol>
-  </div>
+    <div class="container">
+  <div class="row">
+    <h1 class="col">
+Settings    </h1>
+</div>
+<div class="row">
+<p class="col">
+  Here you can edit the notifications you recieve from Elly.
+  We will alert you anytime an incident button press occurs.</p>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+  <label class="form-check-label" for="defaultCheck1">
+Recieve a notification anytime a signle button press occurs  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+  <label class="form-check-label" for="defaultCheck1">
+Recieve a notification anytime a double button press occurs  </label>
+</div>
+<div class="row">
+<p class="col">
+    sex</p>
+</div>
+</div>
+</div>
 </template>
 
 <script>
